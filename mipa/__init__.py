4295f88739b02ac983727edfe85bd4b89106790b
yupix <yupi0982@outlook.jp>
2022-04-27 19:11:50 +0900

    feat: bump version

--- mipa/__init__.py
+++ mipa/__init__.py
@@ -3,7 +3,7 @@ __author__ = 'yupix'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2022-present yupix'
 __author_email__ = 'yupi0982@outlook.jp'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ packages = ['mipa', 'mipa.ext', 'mipa.ext.commands', 'mipa.ext.tasks']
 
 setup(
     name='mipa',
-    version='0.0.1',
+    version='0.0.2',
     install_requires=requirements,
     url='https://github.com/yupix/MiPA',
     author='yupix',
