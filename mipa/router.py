12b0593ea6ea47c82ab83370358ac39d82e9beb5
yupix <yupi0982@outlook.jp>
2022-12-23 15:42:42 +0000

    ✨ feat: ノートをキャプチャする際何個までキャプチャするか制限出来るように

--- mipa/router.py
+++ mipa/router.py
@@ -34,8 +34,12 @@ class Router:
         与えられたメッセージを元にnote idを取得し、そのメッセージをon_message等の監視対象に追加します
     """
 
-    def __init__(self, web_socket: ClientWebSocketResponse):
+    def __init__(
+        self, web_socket: ClientWebSocketResponse, max_capure: int = 100
+    ):
         self.web_socket: ClientWebSocketResponse = web_socket
+        self.captured_note: list[str] = []
+        self.max_capture: int = max_capure
 
     async def connect_channel(
         self,
@@ -72,15 +76,20 @@ class Router:
         except KeyError:
             pass
 
-    async def capture_message(self, message_id: str) -> None:
+    async def capture_message(self, note_id: str) -> None:
         """
         与えられたメッセージを元にnote idを取得し、そのメッセージをon_message等の監視対象に追加します
 
         Parameters
         ----------
-        message_id : str
+        note_id : str
         """
-
+        if len(self.captured_note) > self.max_capture:
+            await self.web_socket.send_json(
+                {'type': 'unsubNote', 'body': {'id': f'{note_id}'}}
+            )
+            del self.captured_note[0]
+        self.captured_note.append(note_id)
         await self.web_socket.send_json(
-            {'type': 'subNote', 'body': {'id': f'{message_id}'}}
+            {'type': 'subNote', 'body': {'id': f'{note_id}'}}
         )
