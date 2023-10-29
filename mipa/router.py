48bf3808ebab36897be4e963c5b587c9e1e4d5cb
yupix <yupi0982@outlook.jp>
2023-10-30 07:10:51 +0900

    feat: routerで切断を行えるように close #31
    feat: connect_channelでhandlerを渡せるように

--- mipa/router.py
+++ mipa/router.py
@@ -1,92 +1,103 @@
-"""Misskeyのチャンネルへの接続や、メッセージのキャプチャ等のWebSocket関連"""
-
 from __future__ import annotations
 
 import uuid
-from typing import TYPE_CHECKING, Iterable, Literal
+from typing import TYPE_CHECKING, Iterable, Literal, overload
+
 
 if TYPE_CHECKING:
+    from mipa.ext.timelines.core import AbstractTimeline
     from aiohttp.client_ws import ClientWebSocketResponse
 
 __all__ = ["Router"]
 
+IChannel = Literal["global", "main", "home", "local", "hybrid"]
+
+
+CHANNELS = {
+    "global": "globalTimeline",
+    "main": "main",
+    "home": "homeTimeline",
+    "local": "localTimeline",
+    "hybrid": "hybridTimeline",
+}
+
 
 class Router:
-    """
-    Attributes
-    ----------
-    web_socket : ClientWebSocketResponse
-        WebSocketクライアント
-
-    Methods
-    -------
-    channels:
-        与えられたlistを元にチャンネルに接続します
-    global_time_line:
-        WebSocketでGlobalTimeLineに接続します
-    main_channel:
-        WebSocketでMainチャンネルに接続します
-    home_time_line:
-        WebSocketでHomeTimeLineに接続します
-    local_time_line:
-        WebSocketでLocalTimeLineに接続します
-    hybrid_time_line:
-        WebSocketでHybridTimeLineに接続します
-    capture_message:
-        与えられたメッセージを元にnote idを取得し、そのメッセージをon_message等の監視対象に追加します
-    """
-
     def __init__(
         self, web_socket: ClientWebSocketResponse, max_capure: int = 100
     ):
         self.web_socket: ClientWebSocketResponse = web_socket
         self.captured_note: list[str] = []
         self.max_capture: int = max_capure
+        self.__channel_ids: dict[str, IChannel] = {}
+        self.__channel_handlers: dict[str, AbstractTimeline] = {}
+
+    @overload
+    async def connect_channel(self, channel_list: Iterable[IChannel]):
+        ...
+
+    @overload
+    async def connect_channel(self, channel_list: dict[IChannel, AbstractTimeline|None]):
+        ...
 
     async def connect_channel(
         self,
-        channel_list: Iterable[
-            Literal["global", "main", "home", "local", "hybrid"]
-        ],
-    ) -> None:
+        channel_list: Iterable[IChannel] | dict[IChannel, AbstractTimeline|None],
+    ):
         """
-        与えられたlistを元にチャンネルに接続します
+        Connects to a channel based on the list passed.
 
         Parameters
         ----------
-        channel_list : Iterable[
-                           Literal['global', 'main', 'home', 'local', 'hybrid']
-                       ]
+        channel_list : IChannel
             ['global', 'main', 'home', 'local', 'hybrid']
+
+        Returns
+        -------
+        dict[IChannel, str]
         """
 
-        channel_dict = {
-            "global": "globalTimeline",
-            "main": "main",
-            "home": "homeTimeline",
-            "local": "localTimeline",
-            "hybrid": "hybridTimeline",
-        }
+        _channel_ids: dict[IChannel, str] = {}
         try:
             for channel in channel_list:
-                get_channel = channel_dict[channel]
+                channel_id = f"{uuid.uuid4()}"
+                _channel_ids[channel] = channel_id
+                self.__channel_ids[channel_id] = channel
+                if isinstance(channel_list, dict):
+                    channel_handler = channel_list[channel]
+                    if channel_handler:
+                        self.__channel_handlers[channel_id] = channel_handler
+
                 await self.web_socket.send_json(
                     {
                         "type": "connect",
                         "body": {
-                            "channel": f"{get_channel}",
-                            "id": f"{uuid.uuid4()}",
+                            "channel": f"{CHANNELS[channel]}",
+                            "id": f"{_channel_ids[channel]}",
                         },
                     }
                 )
-
         except KeyError:
             pass
+        return _channel_ids
+
+    async def disconnect_channel(self, channel_id: str):
+        """
+        Disconnects from a channel based on the id passed.
+
+        Parameters
+        ----------
+        channel_id : str
+        """
+
+        self.__channel_ids.pop(channel_id)
+        await self.web_socket.send_json(
+            {"type": "disconnect", "body": {"id": f"{channel_id}"}}
+        )
 
     async def capture_message(self, note_id: str) -> None:
         """
-        与えられたメッセージを元にnote idを取得し、そのメッセージをon_message等の監視対象に追加します
-
+        Captures a message based on the id passed.
         Parameters
         ----------
         note_id : str
@@ -100,3 +111,25 @@ class Router:
         await self.web_socket.send_json(
             {"type": "subNote", "body": {"id": f"{note_id}"}}
         )
+
+    @property
+    def channel_ids(self) -> dict[str, IChannel]:
+        """
+        Returns the unique ID of the connected channel and the channel mapping
+
+        Returns
+        -------
+        dict[str, IChannel]
+        """
+        return self.__channel_ids
+
+    @property
+    def channel_handlers(self) -> dict[str, AbstractTimeline]:
+        """
+        Returns the unique ID of the connected channel and the channel mapping
+
+        Returns
+        -------
+        dict[str, AbstractTimeline]
+        """
+        return self.__channel_handlers
